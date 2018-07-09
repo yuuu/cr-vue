c372cf016c81ad0429c44d990f55d46fd07f98e9
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-07-10 06:46:48 +0900

    S04-S06 complete

--- S04/main.js
+++ S04/main.js
@@ -1,4 +1,15 @@
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    count: 0,
+    list: ['りんご', 'バナナ', 'いちご'],
+    show: false
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target)
+    }
+  }
 })
 
